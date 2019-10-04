7668de199cfad66038b4749f861c19aa08c800d2
Daniele Araújo <37555137+felurye@users.noreply.github.com>
2019-10-04 08:49:30 -0300

    Update 1035.c

--- questions-c/1035.c
+++ questions-c/1035.c
@@ -10,10 +10,10 @@ int main(){
 	
 	scanf("%d%d%d%d",&A,&B,&C,&D);
 	if((B>C) && (D>A) && ((C+D)>(A+B)) && (C>0) && (D>0) && (A%2==0)){
-    	 printf("Valores aceitos\n");
+    		printf("Valores aceitos\n");
     	}
 	else{
-     	printf("Valores nao aceitos\n");
+     		printf("Valores nao aceitos\n");
 	}
 	return 0;
 }
