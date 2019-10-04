d95c3927f2785227570ad4b63a05ac35a8af1988
Daniele Araújo <37555137+felurye@users.noreply.github.com>
2019-10-04 08:45:59 -0300

    Update 1019.c

--- questions-c/1019.c
+++ questions-c/1019.c
@@ -1,3 +1,6 @@
+/* Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica,
+e informe-o expresso no formato horas:minutos:segundos.
+*/
 #include <stdio.h>
 int main(){
 	int hora, min, seg;
