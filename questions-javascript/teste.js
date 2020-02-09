874d33672a29d1b5f67cc78ec07f08056651e9de
Daniele Araújo <37555137+felurye@users.noreply.github.com>
2020-02-09 20:59:15 -0300

    Delete teste.js

--- questions-javascript/teste.js
+++
@@ -1,7 +0,0 @@
-var input = require('fs').readFileSync('./dev/stdin/file.txt', 'utf8');
-var lines = input.split('\n');
-
-var texto = "joão 1444?";
-var er = /[^a-z]/gi;
-texto = texto.replace(er, "");
-console.log(texto);
